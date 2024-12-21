3bd302789760b5ad4c4a6c4609a9ca344f4fc31b
jamesd <dawsonjames027@gmail.com>
2024-12-21 12:56:59 +0000

    quicksave

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './sass/pages/App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +9,6 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
