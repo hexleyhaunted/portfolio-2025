ceeb4aae17e2ddf721233c0b3df82c09359a36b7
jamesd <dawsonjames027@gmail.com>
2024-12-22 18:21:15 +0000

    quicksave

--- portfolio/src/components/technologiesSection/technologyBlock/RatingsColumn.tsx
+++ portfolio/src/components/technologiesSection/technologyBlock/RatingsColumn.tsx
@@ -1,16 +1,23 @@
 import {getCvTechnologyRatingByName} from "../../../utils/cvDataUtils.ts";
+import {getRandomFloat} from "../../../utils/math.ts";
 
-function RatingBlock({isFilled}: {isFilled: boolean}) {
+function RatingBlock({isFilled, delay}: {isFilled: boolean, delay: number}) {
     const filledClass = isFilled ? "filled" : "empty"
-    return <div className={`rating-block ${filledClass}`}></div>
+    return <>
+        <div className={`rating-block`}>
+            <div className={`rating-loading-block ${filledClass}`} style={{animationDelay: `${delay}ms`}}></div>
+        </div>
+    </>
 }
 
-export default function RatingsColumn({technologyName}: {technologyName: string}) {
+export default function RatingsColumn({technologyName}: { technologyName: string}) {
     const rating = getCvTechnologyRatingByName(technologyName) || 0;
     const ratingArray = Array(5).fill(0);
+    const delay = getRandomFloat(2500, 3000);
+
     return (
         <div className={"ratings-column"}>
-            {ratingArray.map((_item, index) => (<RatingBlock isFilled={5 - rating <= index}/>))}
+            {ratingArray.map((_item, index) => (<RatingBlock isFilled={5 - rating <= index} delay={delay - (index * getRandomFloat(200, 500))}/>))}
         </div>
     )
 }
