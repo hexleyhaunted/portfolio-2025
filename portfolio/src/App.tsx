0a07d3164285293aa7624d860513fa8311f5cb28
jamesd <dawsonjames027@gmail.com>
2024-12-22 18:42:50 +0000

    quicksave

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,14 +1,10 @@
 import './sass/pages/App.scss'
-import TechnologyBlock from "./components/technologiesSection/technologyBlock";
-import {getAllCvTechnologyNames} from "./utils/cvDataUtils.ts";
+import TechnologiesSection from "./components/technologiesSection";
 
 function App() {
-
-    const technologyNames = getAllCvTechnologyNames();
-
   return (
     <div className={"sandbox"}>
-        {technologyNames.map((name) => (<TechnologyBlock technologyName={name}/>))}
+        <TechnologiesSection />
     </div>
   )
 }
